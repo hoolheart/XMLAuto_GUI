77420d8fe3dfe4ec166abbb39dd0be259ddec9e4
Edward Chou <prettyage.new@gmail.com>
2014-09-02 21:30:19 +0800

    select file

--- dialog.cpp
+++ dialog.cpp
@@ -1,14 +1,28 @@
 #include "dialog.h"
 #include "ui_dialog.h"
+#include <QFileDialog>
 
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Dialog)
 {
     ui->setupUi(this);
+    connect(ui->scanButton,SIGNAL(clicked()),this,SLOT(selectFileName()));
+    fileName = tr("");
+    ui->pathEdit->setText(fileName);
 }
 
 Dialog::~Dialog()
 {
     delete ui;
+}
+
+void Dialog::selectFileName()
+{
+    QFileDialog dlg;
+
+    if(dlg.exec()==QDialog::Accepted) {
+        fileName = dlg.selectedFiles().at(0);
+        ui->pathEdit->setText(fileName);
+    }
 }
--- dialog.h
+++ dialog.h
@@ -11,12 +11,18 @@ class Dialog : public QDialog
 {
     Q_OBJECT
 
+private:
+    QString fileName;
+
 public:
     explicit Dialog(QWidget *parent = 0);
     ~Dialog();
 
 private:
     Ui::Dialog *ui;
+
+public slots:
+    void selectFileName();
 };
 
 #endif // DIALOG_H
